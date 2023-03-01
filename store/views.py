589e187cd03279259e214c81587eab6074aa3acf
unknown <sarikkenjaev2002@gmail.com>
2023-03-01 14:31:11 +0500

    111 урок

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from carts.views import _cart_id
 from category.models import Category
 from orders.models import OrderProduct
 from store.forms import ReviewForm
-from store.models import Product, ReviewRating
+from store.models import Product, ReviewRating, ProductGallery
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib import messages
 
@@ -52,11 +52,14 @@ def product_detail(request, category_slug, product_slug):
     # get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
 
     return render(request, 'store/product-detail.html', context)
