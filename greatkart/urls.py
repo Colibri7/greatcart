d971a00286960eafd9655c7ed4aab769ec4ecf5e
unknown <sarikkenjaev2002@gmail.com>
2023-03-01 17:18:27 +0500

    the end

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -5,7 +5,9 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+
+    path('securelogin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
@@ -13,7 +15,6 @@ urlpatterns = [
     # ORDERS
     path('orders/', include('orders.urls')),
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
